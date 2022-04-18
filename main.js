f21488c89bd79bb335c7b22b9e8f319836a120cd
Audry Wolters <audrywolters@gmail.com>
2022-04-18 18:39:22 -0500

    v-for! also giving each item an id. fancy. jk

--- main.js
+++ main.js
@@ -2,10 +2,22 @@ const app = Vue.createApp({
 	data() {
 		return {
 			product: 'Socks',
-			image: './assets/images/socks_blue.jpg',
-			inventory: 11,
-			onSale: false
-			// description: 'Really Cool Socks',
+			image: './assets/images/socks_green.jpg',
+            inStock: true,
+            details: [
+				'50% cotton', 
+				'30% wool', 
+				'20% polyester'
+			],
+			variants: [
+				{ id: 2234, color: 'green'},
+				{ id: 2235, color: 'blue'},
+			],
+
+
+			// inventory: 11,
+			// onSale: true
+			// description: 'Really Cool Socks'
 			// url: 'https://github.com/audrywolters'
 		}
 	}

08d020d852ae150c513e18268ead3d2b52c2c072
Audry Wolters <audrywolters@gmail.com>
2022-04-19 14:47:55 -0500

    getting ready for #6 event handling

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			cart: 0,
 			product: 'Socks',
-			image: './assets/images/socks_green.jpg',
-            inStock: true,
-            details: [
-				'50% cotton', 
-				'30% wool', 
-				'20% polyester'
-			],
+			image: './assets/images/socks_blue.jpg',
+			inStock: true,
+			details: ['50% cotton', '30% wool', '20% polyester'],
 			variants: [
-				{ id: 2234, color: 'green'},
-				{ id: 2235, color: 'blue'},
-			],
-			sizes: [
-				{ id: 100, type: 'small'},
-				{ id: 101, type: 'medium'},
-				{ id: 102, type: 'large'},
-			],
+				{ id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+				{ id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+			]
 
-
+			// sizes: [
+			// 	{ id: 100, type: 'small'},
+			// 	{ id: 101, type: 'medium'},
+			// 	{ id: 102, type: 'large'},
+			// ],
 			// inventory: 11,
 			// onSale: true
 			// description: 'Really Cool Socks'

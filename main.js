1dad264a874cb0f7f2f02bd8f7989836d1106829
Audry Wolters <audrywolters@gmail.com>
2022-04-21 10:08:55 -0500

    changed data properties (sockImage and inStock) into computed properties

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ const app = Vue.createApp({
 			cart: 0,
 			product: 'Socks',
             brand: 'Vue Mastery',
-			sockImage: './assets/images/socks_blue.jpg',
-			inStock: true,
+			selectedVariant: 0,
 			details: ['50% cotton', '30% wool', '20% polyester'],
 			variants: [
-				{ id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-				{ id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+				{ id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+				{ id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg',  quantity: 0 },
 			]
 
 			// sizes: [
@@ -27,14 +26,24 @@ const app = Vue.createApp({
 		addToCart() {
 			this.cart += 1
 		},
-		updateImage(variantImage) {
-			this.sockImage = variantImage
+		updateVariant(index) {
+			this.selectedVariant = index
 		},
-
 		// removeFromCart() {
 		// 	if (this.cart > 0) {
 		// 		this.cart -= 1
 		// 	}
 		// },
+	},
+	computed: {
+		title() {
+			return this.brand + ' ' + this.product
+		},
+		sockImage() {
+			return this.variants[this.selectedVariant].image
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].quantity
+		}
 	}
 })
